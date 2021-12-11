b0f47be574e8b047dbe00dc0aeac73496ffbb06c
Benedetta52 <91663828+Benedetta52@users.noreply.github.com>
2021-12-11 20:33:57 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,16 +4,15 @@
 using namespace std;
 
 int main(){
-  int dim;
-  cin >> dim;
-  float numeri[dim];
+   int dim;
+   cin >> dim;
+   float numeri[dim];
+   for (int i = 0 ; i < dim ; i++){
+       cin >> numeri[i];
+   } 
+   numeroMassimo(numeri, dim);
 
-  for (int i=0; i < dim; i++){
-    cin >> numeri[i];
-  }
-  numeroMassimo(numeri, dim);
-  
-  cout << numeroMassimo(numeri, dim) << endl;
+        cout << numeroMassimo(numeri , dim ) << endl;
   
   return 0;
 }
